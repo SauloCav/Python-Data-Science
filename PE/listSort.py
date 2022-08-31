347f46d576aa8c09be5eea65ece91e9dc6394f31
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-08-31 06:40:36 -0300

    Create listSort.py

---
+++ PE/listSort.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import statistics as stt
+import numpy as np
+import matplotlib.pyplot as plt
+
+list = [53.0 53.4 95.4 53.5 72.3 70.2
+82.5 51.1 64.3 59.5 84.3 67.3
+74.4 82.7 55.3 69.5 54.1 55.7
+78.5 73.0 77.8 70.5 63.5 55.7
+52.4 87.5 71.4 85.8 69.1 50.7]
+
+list.sort()
