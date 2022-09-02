8826208f317f66dcca84cf4aa9e2cd76d5cb9522
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-09-02 07:40:34 -0300

    Create 01_06.py

---
+++ PE/Actvs/01_06.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import statistics as stt
+import numpy as np
+import matplotlib.pyplot as plt
+
+pos=["L&M", "CE", "F&N", "A&N"]
+lista=[6, 4, 7, 5]
+
+plt.bar(pos, lista)
+plt.show()
